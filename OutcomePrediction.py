7dd986a1b3b8d795c37de20eb5b661b097f278cf
Evgeniy Umanskiy <evgeniyu@sensus.co.il>
2020-06-17 14:56:26 +0300

    Cross validation

--- OutcomePrediction.py
+++ OutcomePrediction.py
@@ -1,7 +1,9 @@
 import sqlite3
 import pandas as pd
 import numpy as np
-import sklearn as skl
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold
 
 
 def add_winner_label(matches):
@@ -52,10 +54,30 @@ def get_player_overall_rating(matches, player_stats):
             matches.at[index, player] = current_stats['overall_rating']
 
     # rename player feature to player + _overall_rating
-    for player in players:
-        matches = matches.rename(columns={player: player + '_overall_rating'})
+    # for player in players:
+    #     matches = matches.rename(columns={player: player + '_overall_rating'})
 
     return matches
+
+
+def classification_model(model, data, predictors, outcome):
+    model.fit(data[predictors], data[outcome])
+
+    predictions = model.predict(data[predictors])
+
+    accuracy = metrics.accuracy_score(predictions, data[outcome])
+    print("Training accuracy : %s" % "{0:.3%}".format(accuracy))
+
+    kf = KFold(n_splits=10)
+    accuracy = []
+    for train, test in kf.split(data):
+        train_predictors = (data[predictors].iloc[train, :])
+        train_target = data[outcome].iloc[train]
+        model.fit(train_predictors, train_target)
+        accuracy.append(model.score(data[predictors].iloc[test, :], data[outcome].iloc[test]))
+
+    print("Cross-Validation Score : %s" % "{0:.3%}".format(np.mean(accuracy)))
+    model.fit(data[predictors], data[outcome])
 
 
 # Connection to DB
@@ -84,3 +106,18 @@ matches_data = get_player_overall_rating(matches_data, players_stats_data)
 matches_data = add_winner_label(matches_data)
 
 print(matches_data.to_string())
+
+# Random Forest Algorithm
+outcome_var = "winner_id_label"
+raw_model = RandomForestClassifier(n_estimators=100)
+predictor_var = ['away_team_goal', 'home_player_1', 'home_player_2',
+                 'home_player_3', 'home_player_4', 'home_player_5', 'home_player_6', 'home_player_7',
+                 'home_player_8', 'home_player_9', 'home_player_10', 'home_player_11', 'away_player_1',
+                 'away_player_2', 'away_player_3', 'away_player_4', 'away_player_5', 'away_player_6',
+                 'away_player_7', 'away_player_8', 'away_player_9', 'away_player_10', 'away_player_11',
+                 'B365H', 'B365D', 'B365A', 'BWH', 'BWD', 'BWA', 'IWH', 'IWD', 'IWA', 'LBH', 'LBD', 'LBA', 'PSH', 'PSD',
+                 'PSA',
+                 'WHH', 'WHD', 'WHA', 'SJH', 'SJD', 'SJA', 'VCH', 'VCD', 'VCA', 'GBH', 'GBD', 'GBA', 'BSH', 'BSD',
+                 'BSA']
+
+classification_model(raw_model, matches_data, predictor_var, outcome_var)
